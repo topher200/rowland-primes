05c7674ec2f44a6eb6a5fa1ebdb48ceba37abe31
Topher <topher200@gmail.com>
2010-11-19 01:50:09 -0500

    wrote gcd(). class currently includes a println() statement calling it

--- src/rowland_primes/core.clj
+++ src/rowland_primes/core.clj
@@ -1,1 +1,9 @@
 (ns rowland_primes.core)
+(defn gcd 
+  "Returns the greatest common denominator of two ints"
+  [a b]
+  (if (> a b) (gcd b a) ; make sure a < b
+      (if (= a 0) b ; if a == 0, return b
+	  (gcd b (mod a b)))))
+
+(println (str (gcd 5 0)))
