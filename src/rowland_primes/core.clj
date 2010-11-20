b50a4ceb8c851f5915ed539171aafa4af1672032
Topher <topher200@gmail.com>
2010-11-20 18:31:19 -0500

    comment nit

--- src/rowland_primes/core.clj
+++ src/rowland_primes/core.clj
@@ -3,8 +3,8 @@
 (defn gcd 
   "Returns the greatest common denominator of two ints"
   [a b]
-  (if (> a b) (gcd b a) ; make sure a < b
-      (if (= a 0) b ; if a == 0, return b
+  (if (> a b) (gcd b a)  ;; make sure a < b
+      (if (= a 0) b  ;; if a == 0, return b
 	  (gcd b (mod a b)))))
 
 (println (str (gcd 5 0)))
