3f2b7961c9991a984755c1f67c31fc3360366a4f
Topher <topher200@gmail.com>
2010-11-20 20:04:18 -0500

    wrote a unit test for gcd

--- test/rowland_primes/test/core.clj
+++ test/rowland_primes/test/core.clj
@@ -2,5 +2,10 @@
   (:use [rowland_primes.core] :reload)
   (:use [clojure.test]))
 
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
+(deftest gcd-test
+  (is (= 5 (gcd 5 0)))
+  (is (= 12 (gcd 12 24)))
+  (is (= 12 (gcd 24 12)))
+  (is (= 12 (gcd 48 12)))
+  (is (= 1 (gcd 3 7)))
+  )
