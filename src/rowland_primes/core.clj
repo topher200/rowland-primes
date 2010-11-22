7749d2509c9f602111fa8daccf533fcc04324a64
Topher <topher200@gmail.com>
2010-11-22 00:33:25 -0500

    implementation for sequence #1. passes unit tests

--- src/rowland_primes/core.clj
+++ src/rowland_primes/core.clj
@@ -1,8 +1,19 @@
 (ns rowland_primes.core)
 
+(declare seq1)
+
 (defn gcd 
   "Returns the greatest common denominator of two ints"
   [a b]
   (if (> a b) (gcd b a)  ;; make sure a < b
       (if (= a 0) b  ;; if a == 0, return b
 	  (gcd a (mod b a)))))
+
+(defn algo1
+  "Algorithm for the first sequence of the problem"
+  ([n]
+     (if (= n 1) 7
+	 (+ (nth seq1 (- n 2)) (gcd n (nth seq1 (- n 2))))))) ;; n-2 since we're starting at 1
+
+(def seq1
+     (map algo1 (iterate inc 1)))
