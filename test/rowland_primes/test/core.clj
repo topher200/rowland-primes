01f6b5fd16cf3caf33e1e5d2ac31b7f97083d520
Topher <topher200@gmail.com>
2010-11-22 00:37:02 -0500

    using the local 'let' instead of the global 'def'

--- test/rowland_primes/test/core.clj
+++ test/rowland_primes/test/core.clj
@@ -14,7 +14,6 @@
   (is (= 8 (algo1 2))))
 
 (deftest seq1-test
-  ;; todo topher- use let instead
-  (def test-list '(7, 8, 9, 10, 15, 18, 19, 20, 21, 22, 33, 36, 37,
-       38, 39, 40, 41, 42, 43, 44, 45, 46, 69, 72, 73))
-  (is (= test-list (take (count test-list) seq1))))
+  (let [test-list '(7, 8, 9, 10, 15, 18, 19, 20, 21, 22, 33, 36, 37,
+       38, 39, 40, 41, 42, 43, 44, 45, 46, 69, 72, 73)]
+    (is (= test-list (take (count test-list) seq1)))))
