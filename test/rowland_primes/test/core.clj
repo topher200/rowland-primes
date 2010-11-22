10d834049f6172ccdb1d560f96a99bfb39bb4fe2
Topher <topher200@gmail.com>
2010-11-21 23:52:48 -0500

    unit test for seq1. Hurray TDD

--- test/rowland_primes/test/core.clj
+++ test/rowland_primes/test/core.clj
@@ -12,3 +12,9 @@
 (deftest algo1-test
   (is (= 7 (algo1 1)))
   (is (= 8 (algo1 2))))
+
+(deftest seq1-test
+  ;; todo topher- use let instead
+  (def test-list '(7, 8, 9, 10, 15, 18, 19, 20, 21, 22, 33, 36, 37,
+       38, 39, 40, 41, 42, 43, 44, 45, 46, 69, 72, 73))
+  (is (= test-list (take (count test-list) seq1))))
